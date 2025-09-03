c1975f65d3d44566cbd5bedb9ab1c6c02a6f83fa
Davi Dantas <davidantasdev@gmail.com>
2025-09-03 20:03:09 -0300

    initial commit

---
+++ posOuNeg/scriptX.php
@@ -0,0 +1,10 @@
+<?php
+$numero = $_REQUEST['numero'];
+if ($numero > 0) {
+    echo "O número $numero é positivo.";
+} elseif ($numero < 0) {
+    echo "O número $numero é negativo.";
+} else {
+    echo "O número é zero.";
+}
+?>
